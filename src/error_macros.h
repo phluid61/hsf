76367ce083f38487f302c4017e47cc11b77d6740
Matthew Kerwin <matthew.kerwin@qut.edu.au>
2020-09-18 16:36:49 +1000

    remove comment

--- src/error_macros.h
+++ src/error_macros.h
@@ -1,7 +1,6 @@
 #ifndef _ERROR_MACROS_H_
 #define _ERROR_MACROS_H_
 
-/* FIXME: __cascade could be better ... */
 #define __cascade(v,x)  { if ((int*)0 != (v) && 0 != *(v)) return x; }
 #define __clear(v)      { if ((int*)0 != (v)) *(v) = SH_E_NO_ERROR; }
 #define __raise(v,code) { if ((int*)0 != (v)) *(v) = (code); }
